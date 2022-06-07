8ab4bbbec58d4521e920c00168bf1ecedf09e769
gabrielforster <rochafrgabriel@gmail.com>
2022-06-06 22:32:49 -0300

    feat: Init on express server

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
-const instaTouch = require('instatouch');
-const fs = require('fs');
-require('dotenv').config();
+const express = require('express')
+const app = express()
+app.use(express.json())
 
-/*
-this function gets the comments from a instagram post
-*/
+const sorter = require('./sorter.js')
 
-async function collectComments() {
-    try {
-        const options = { 
-            count: 100, //count should be the amount of comments present on the post
-            session: process.env.INSTAGRAM_SESSION_ID,
-        };
-
-        /* instaTouch.comments needs an string of the "id" of the post, it's present on the instagram link. So https://www.instagram.com/p/B7wOyffArc5/ -> post id  = 'B7wOyffArc5' */
-        const comments = await instaTouch.comments('B7wOyffArc5'/*<-- Here you need to put the link of the instagram post*/, options);
-        return(comments.collector);
-    } catch (error) {
-        console.log(error);
+//B7wOyffArc5
+app.get('/:id', async (req, res) => {
+    const postId = req.params.id
+    const sorted = await sorter(postId)
+    winner = {
+        id: postId,
+        sortedAt: new Date().toLocaleDateString(),
+        postWinner: sorted
     }
-}
+    res.json(winner)
+})
 
-const pickWin = (comments) => {
-    const pickAComment = Math.floor(Math.random() * comments.length)
-    const picked = comments[pickAComment]
-    console.log(picked)
-    return(picked)
-}
-
-const saveWinner = (winner) => {
-    fs.writeFile('winner.json', JSON.stringify(winner, null, 1), function(err){
-        if (err) console.log(err)
-    })
-}
-
-async function main () {
-    const commentsCollected = await collectComments()
-    const pickedWinner = pickWin(commentsCollected)
-    saveWinner(pickedWinner)
-}
-
-main()
+app.listen(
+    3000,
+    () => {
+        console.log(
+            '\nServer running on port 3000!',
+            '\nVisit localhost:3000/{postId} to get the winner of the post'
+            )
+    }
+)
